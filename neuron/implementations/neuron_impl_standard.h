f5ec2877bca52156a3a7fc137a59715863458716
mattapattu <mattpattu@gmail.com>
2020-08-11 11:09:17 +0200

    new

--- neuron/implementations/neuron_impl_standard.h
+++ neuron/implementations/neuron_impl_standard.h
@@ -280,6 +280,7 @@ static int32_t  neuron_impl_neuron_update(uint32_t time, index_t neuron_index,
     int32_t ret = 1;
     while(ret == 1){
         log_info("Calling neuron_model_PDevs_sim");
+        log_info("neuron %u: tl = %u", neuron_index, neuron->tl);
         ret = neuron_model_PDevs_sim(neuron, threshold_type, nextSpikeTime, key, neuron_index, input);
         log_info("neuron_model_PDevs_sim returns %u", ret);
     }
