a2488d409965187300a73b8f82a9b574ef3cfd35
mattapattu <mattpattu@gmail.com>
2020-08-11 16:10:46 +0200

    upd

--- neuron/models/neuron_model.h
+++ neuron/models/neuron_model.h
@@ -102,9 +102,9 @@ void neuron_model_eit_update(neuron_pointer_t neuron, uint32_t time);
 
 int32_t deltaInt(neuron_t * neuron);
 
-int32_t deltaExt(neuron_t * neuron, uint32_t time, uint32_t threshold, input_t input);
+int32_t deltaExt(neuron_t * neuron, uint32_t time, int32_t threshold, input_t input);
 
-void neuron_model_Devs_sim(neuron_t * neuron, int16_t event_type, uint32_t nextSpikeTime, uint32_t threshold, key_t key, uint32_t neuron_index, input_t input);
+void neuron_model_Devs_sim(neuron_t * neuron, int16_t event_type, uint32_t nextSpikeTime, int32_t threshold, key_t key, uint32_t neuron_index, input_t input);
 
 void neuron_model_init(neuron_t * neuron);
 
