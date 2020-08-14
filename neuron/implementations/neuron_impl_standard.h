ba89c491917440909229346b06e05f5ae309c3fc
mattapattu <mattpattu@gmail.com>
2020-08-14 14:28:35 +0200

    test

--- neuron/implementations/neuron_impl_standard.h
+++ neuron/implementations/neuron_impl_standard.h
@@ -263,7 +263,7 @@ static bool neuron_impl_check_sim_end(uint32_t n_neurons){
         for (index_t n = 0; n < n_neurons; n++) {
         if(neuron_model_get_phase(&neuron_array[n]) == 4){
             endSim = endSim && true;
-        }else if(neuron_model_get_phase(&neuron_array[n]) == 5){ // Remove ERR ?
+        }else if(neuron_model_get_phase(&neuron_array[n]) == 5){ // Remove ERR ? Check later.
             err = true;
             break;
         }
