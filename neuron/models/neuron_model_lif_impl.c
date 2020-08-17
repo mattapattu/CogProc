0df0a05ae807af87def43fe35d7774086ae27e60
mattapattu <mattpattu@gmail.com>
2020-08-17 15:10:19 +0200

    test

--- neuron/models/neuron_model_lif_impl.c
+++ neuron/models/neuron_model_lif_impl.c
@@ -92,15 +92,16 @@ static void lambda(neuron_t * neuron, key_t key, uint32_t neuron_index, bool use
             //time  = time + neuron->tn;
             //set 32nd bit if packet is eot messg. 
             
+            log_info("Sending EOT with key = %u, neuron_index = %u, time = %u",key,  neuron_index, nextEventTime );
             nextEventTime |= (1 << 31);
-            log_info("Sending EOT with key = %u, neuron_index = %u, payload = %u",key,  neuron_index, nextEventTime );
             while (!spin1_send_mc_packet(
                             key | neuron_index, nextEventTime, WITH_PAYLOAD)) {
                         spin1_delay_us(1000);
                     }
         }else if(currentState == 0||currentState == 1){
+            log_info("Sending EOT with key = %u, neuron_index = %u, time = %u",key,  neuron_index, nextEventTime );
             nextEventTime |= (1 << 31);
-            log_info("Sending EOT with key = %u, neuron_index = %u, payload = %u",key,  neuron_index, nextEventTime );
+            //log_info("Sending EOT with key = %u, neuron_index = %u, payload = %u",key,  neuron_index, nextEventTime );
             while (!spin1_send_mc_packet(
                             key | neuron_index, nextEventTime, WITH_PAYLOAD)) {
                         spin1_delay_us(1000);
