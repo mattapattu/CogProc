34c8fbb25cbf0cc4ce5296851dc63a9181bed20e
mattapattu <mattpattu@gmail.com>
2020-08-19 16:18:54 +0200

    test

--- neuron/spike_processing.c
+++ neuron/spike_processing.c
@@ -205,7 +205,7 @@ static void setup_synaptic_dma_read(dma_buffer *current_buffer,
     bool setup_done = false;
     while (!setup_done && is_something_to_do(&row_address,
             &n_bytes_to_transfer, &spike, &dma_n_rewires, &dma_n_spikes, spiketime)) {
-        log_info("While loop: mc_pkt (%u,%u)", spike, *spiketime);        
+        //log_info("While loop: mc_pkt (%u,%u)", spike, *spiketime);        
         /* if (current_buffer != NULL &&
                 current_buffer->sdram_writeback_address == row_address) {
             // If we can reuse the row, add on what we can use it for
@@ -220,7 +220,7 @@ static void setup_synaptic_dma_read(dma_buffer *current_buffer,
          if (n_bytes_to_transfer == 0) {
             //log_info("Processing mc_pkt (%u,%u), row is in DTCM", spike, *spiketime); 
             spike_processing_count++;
-            log_info("spike_processing_count = %u", spike_processing_count);
+            //log_info("spike_processing_count = %u", spike_processing_count);
             // If the row is in DTCM, process the row now
             synaptic_row_t single_fixed_synapse =
                     direct_synapses_get_direct_synapse(row_address);
@@ -242,7 +242,7 @@ static void setup_synaptic_dma_read(dma_buffer *current_buffer,
     if(setup_done){
         //log_info("Processing mc_pkt (%u,%u) after DMA read", spike, *spiketime); 
         spike_processing_count++;            
-        log_info("spike_processing_count = %u", spike_processing_count);
+        //log_info("spike_processing_count = %u", spike_processing_count);
         // synaptic_row_t single_fixed_synapse =
         //             direct_synapses_get_direct_synapse(row_address);
         uint32_t current_buffer_index = buffer_being_read;
