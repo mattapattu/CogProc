46e6c4f198fe547f13e4eae667d44455495665a8
mattapattu <mattpattu@gmail.com>
2020-08-20 11:04:08 +0200

    test

--- neuron/synapses.c
+++ neuron/synapses.c
@@ -391,23 +391,23 @@ bool synapses_process_synaptic_row(
     // **TODO** multiple optimised synaptic row formats
     //if (plastic_tag(row) == 0) {
     // If this row has a plastic region
-    if (synapse_row_plastic_size(row) > 0) {
-        // Get region's address
-        address_t plastic_region_address = synapse_row_plastic_region(row);
+    // if (synapse_row_plastic_size(row) > 0) {
+    //     // Get region's address
+    //     address_t plastic_region_address = synapse_row_plastic_region(row);
 
-        // Process any plastic synapses
-        profiler_write_entry_disable_fiq(
-                PROFILER_ENTER | PROFILER_PROCESS_PLASTIC_SYNAPSES);
-        if (!synapse_dynamics_process_plastic_synapses(plastic_region_address,
-                fixed_region_address, ring_buffers, time)) {
-            return false;
-        }
-        profiler_write_entry_disable_fiq(
-                PROFILER_EXIT | PROFILER_PROCESS_PLASTIC_SYNAPSES);
+    //     // Process any plastic synapses
+    //     profiler_write_entry_disable_fiq(
+    //             PROFILER_ENTER | PROFILER_PROCESS_PLASTIC_SYNAPSES);
+    //     if (!synapse_dynamics_process_plastic_synapses(plastic_region_address,
+    //             fixed_region_address, ring_buffers, time)) {
+    //         return false;
+    //     }
+    //     profiler_write_entry_disable_fiq(
+    //             PROFILER_EXIT | PROFILER_PROCESS_PLASTIC_SYNAPSES);
 
-        // Perform DMA write back
-        *write_back = true;
-    }
+    //     // Perform DMA write back
+    //     *write_back = true;
+    // }
 
     // Process any fixed synapses
     // **NOTE** this is done after initiating DMA in an attempt
