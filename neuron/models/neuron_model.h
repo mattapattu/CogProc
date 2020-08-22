2116305938c5ebbda5f4b772f19b78c7a4ae5dd7
mattapattu <mattpattu@gmail.com>
2020-08-22 14:26:24 +0200

    test

--- neuron/models/neuron_model.h
+++ neuron/models/neuron_model.h
@@ -91,7 +91,7 @@ int32_t neuron_model_PDevs_sim(neuron_t * neuron, int32_t threshold,  uint32_t n
 
 void neuron_model_Devs_sim(neuron_t * neuron, int16_t event_type, uint32_t nextSpikeTime, int32_t threshold, key_t key, uint32_t neuron_index, input_t input, bool use_key);
 
-int32_t deltaInt(neuron_t * neuron,key_t key, uint32_t neuron_index, bool use_key);
+uint16_t deltaInt(neuron_t * neuron,key_t key, uint32_t neuron_index, bool use_key);
 
 int32_t deltaExt(neuron_t * neuron, uint32_t time, int32_t threshold, input_t input);
 
