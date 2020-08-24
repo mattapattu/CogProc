27351ca60fd30e8cce8f57da70882125818fe174
mattapattu <mattpattu@gmail.com>
2020-08-24 13:59:11 +0200

    test

--- neuron/implementations/neuron_impl_standard.h
+++ neuron/implementations/neuron_impl_standard.h
@@ -317,7 +317,7 @@ static bool  neuron_impl_neuron_update(index_t neuron_index,
     if(nextSpikeTime < INFINITY){
         input = synapses_get_ring_buffer_input(nextSpikeTime,neuron_index ); 
         while(ret == 1){
-        //log_info("Calling neuron_model_PDevs_sim");
+            log_info("Calling neuron_model_PDevs_sim");
             nextSpikeTime = neuron->spike_times[0];
             ret = neuron_model_PDevs_sim(neuron, nextSpikeTime, key, neuron_index, input,use_key);
         
