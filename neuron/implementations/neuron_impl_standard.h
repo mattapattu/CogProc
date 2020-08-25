8e8bc6babc73663cd39fa51dd8ad00f32dc0632e
mattapattu <mattpattu@gmail.com>
2020-08-25 11:23:15 +0200

    test

--- neuron/implementations/neuron_impl_standard.h
+++ neuron/implementations/neuron_impl_standard.h
@@ -264,7 +264,7 @@ static uint32_t neuron_impl_update_spiketime(uint32_t time, index_t neuron_index
         log_info("Changing neuron %u ext. inp to time = %u, lastProcessedSpikeTime = %u", neuron_index,time,neuron->lastProcessedSpikeTime);
     }else if(neuron->lastProcessedSpikeTime - time  == deltaT){ // if lastProcessedSpikeTime is already been shifted once
         time = neuron->lastProcessedSpikeTime + deltaT;
-        log_info("Changing neuron %u ext. inp to time = %u, lastProcessedSpikeTime = %u", neuron_index,time,neuron->lastProcessedSpikeTime);
+        log_info("Updating neuron %u ext. inp to time = %u, lastProcessedSpikeTime = %u", neuron_index,time,neuron->lastProcessedSpikeTime);
     }
     return(time);
 }
