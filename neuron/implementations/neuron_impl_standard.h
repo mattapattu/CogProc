7618c8d97ac8c4d803c627eea1e9af1d9aa62cac
mattapattu <mattpattu@gmail.com>
2020-08-25 22:15:11 +0200

    test

--- neuron/implementations/neuron_impl_standard.h
+++ neuron/implementations/neuron_impl_standard.h
@@ -325,11 +325,10 @@ static bool  neuron_impl_neuron_update(index_t neuron_index,
         input_type_convert_excitatory_input_to_current(
                 &input, input_types, 0);
         log_info("input1 = %u", input);
-        uint32_t nextInp = *input;        
         while(ret == 1){
             //log_info("Calling neuron_model_PDevs_sim");
             nextSpikeTime = neuron->spike_times[0];
-            ret = neuron_model_PDevs_sim(neuron, nextSpikeTime, key, neuron_index, nextInp,use_key);
+            ret = neuron_model_PDevs_sim(neuron, nextSpikeTime, key, neuron_index, input,use_key);
         
             if(neuron_impl_spiked(neuron_index)){
             //log_info("Setting recording bit for neuron %u",neuron_index);
