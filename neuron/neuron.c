ad297b108326a9af34bbd62ff1a11bf6886faf0f
mattapattu <mattpattu@gmail.com>
2020-08-26 22:41:02 +0200

    test

--- neuron/neuron.c
+++ neuron/neuron.c
@@ -222,7 +222,7 @@ bool neuron_pdevs_update(){
             spin1_callback_off(USER_EVENT); 
             spin1_callback_off(DMA_TRANSFER_DONE);
             spin1_delay_us(100);
-            if(!recvd_end_sig){
+            if(recvd_end_sig){
                 neuron_send_terminate_sig(sim_exit_time);
             }
             
