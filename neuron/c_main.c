be1cb7240a2a7405e6155ecb36dab6210ce1ae0c
mattapattu <mattpattu@gmail.com>
2020-08-27 10:47:39 +0200

    test

--- neuron/c_main.c
+++ neuron/c_main.c
@@ -279,8 +279,9 @@ void resume_callback(void) {
 void mc_pkt_recvd_callback(uint key, uint payload) {
     //use(payload);
 
+    spin1_delay_us(100);
    // uint32_t time = payload &  2147483647;
-    log_info("Received mc_pkt (%u,%u)", key, payload);
+    //log_info("Received mc_pkt (%u,%u)", key, payload);
     
     
     neuron_set_recvd_end_sig(payload);
