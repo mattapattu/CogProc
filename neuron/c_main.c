0f25df4e226fb5c6c824a55d7dded4f798e6bec6
mattapattu <mattpattu@gmail.com>
2020-08-27 13:06:58 +0200

    test

--- neuron/c_main.c
+++ neuron/c_main.c
@@ -297,8 +297,8 @@ void end_sim(){
     log_info("Inside end_sim");
     simulation_handle_pause_resume(NULL);
     spin1_delay_us(100);
-    // neuron_recording_finalise();
-    // spin1_delay_us(100);
+    neuron_recording_finalise();
+    spin1_delay_us(100);
     simulation_ready_to_read();
     //spin1_exit(0);
 }
